5f0208f957f84ca729d9f72ccf68b56439530cea
Fernanda260504 <132949457+Fernanda260504@users.noreply.github.com>
2025-03-30 14:45:56 -0600

    [CREATE] creacion del inicio de sesion

--- Hospital/FRONT/HospitalReact/src/main.jsx
+++ Hospital/FRONT/HospitalReact/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import { createRoot } from 'react-dom/client'
+import Login from './Login/Login'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Router>
+    <Routes>
+      <Route path='/Hospital/Login' element={<Login />} />
+      
+    </Routes>
+  </Router>
 )
